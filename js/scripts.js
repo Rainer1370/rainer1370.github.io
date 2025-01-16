62d4600a388ac519b873f72c773ec0575eaff4f0
Rainer <rainer1370@gmail.com>
2025-01-16 11:55:22 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,27 @@
 document.addEventListener("DOMContentLoaded", function () {
     // Include Header
-    fetch('../components/header.html')
-        .then((response) => response.text())
+    fetch("../components/header.html")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to load header: " + response.statusText);
+            }
+            return response.text();
+        })
         .then((data) => {
-            document.getElementById('header').innerHTML = data;
-        });
+            document.getElementById("header").innerHTML = data;
+        })
+        .catch((error) => console.error(error));
 
     // Include Footer
-    fetch('../components/footer.html')
-        .then((response) => response.text())
+    fetch("../components/footer.html")
+        .then((response) => {
+            if (!response.ok) {
+                throw new Error("Failed to load footer: " + response.statusText);
+            }
+            return response.text();
+        })
         .then((data) => {
-            document.getElementById('footer').innerHTML = data;
-        });
+            document.getElementById("footer").innerHTML = data;
+        })
+        .catch((error) => console.error(error));
 });
